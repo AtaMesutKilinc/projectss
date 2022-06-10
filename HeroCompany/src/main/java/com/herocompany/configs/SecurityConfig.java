3de43e612f0129b52f4d740e78cbdb6973f6296b
Ata Mesut Kilinc <katamesut@gmail.com>
2022-06-10 15:14:45 +0300

    Security Confihs -hasRoles

--- HeroCompany/src/main/java/com/herocompany/configs/SecurityConfig.java
+++ HeroCompany/src/main/java/com/herocompany/configs/SecurityConfig.java
@@ -37,19 +37,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable().formLogin().disable();
-        //csrf defaultta gelen login methodunun arkasında gerçekten kendinden geldiğini anlıyor. bunu kapa arama
-        //formu da arama.
+
         http
                 .authorizeHttpRequests() //giriş rolleri ile çalış
-                .antMatchers("/customer/**").permitAll()
-                .antMatchers("/admin/**").permitAll()  //bunlar açık oldu. rol ve giriş şartı aramıyorz.
-//                .antMatchers("/orders/**").hasRole("user")  //hangi servis hangi rolle çalışcak emrini veriyoruz.
-//                .antMatchers("/basket/**").hasRole("admin")   //giriş olac
-//                .antMatchers("/category/**").hasRole("customer")
-                .antMatchers("/orders/**").permitAll()
-                .antMatchers("/basket/**").permitAll()
-                .antMatchers("/category/**").permitAll()
-                .antMatchers("/product/**").permitAll()
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/basket/save","/basket/update","/basket/delete","/basket/MyBaskets").hasRole("customer")  //hangi servis hangi rolle çalışcak emrini veriyoruz.
+                .antMatchers("/basket/list").hasRole("admin")   //giriş olac
+                .antMatchers("/customer/settings","/customer/changePassword").hasRole("customer") //bunlar açık oldu. rol ve giriş şartı aramıyorz.
+                .antMatchers("/customer/**").hasRole("admin") //bunlar açık oldu. rol ve giriş şartı aramıyorz.
+                .antMatchers("/category/list").hasRole("customer")
+                .antMatchers("/category/**").hasRole("admin")
+                .antMatchers("/orders/**").hasRole("admin")
+                .antMatchers("/orders/save", "/orders/MyOrders","/orders/update","/orders/delete").hasRole("customer")
+                .antMatchers("/product/**").hasRole("admin")
+                .antMatchers("/product/list","/product/search","/product/productByCategory").hasRole("customer")
+                .antMatchers("/forgot_password/**","reset_password/**").permitAll()
+                .antMatchers("/registerAdmin/**").hasRole("admin")
+                .antMatchers("/login/**","/register/**").permitAll()
                 .and()       //tanımlar dışında config var onları da koy
                 .csrf().disable()
                 .formLogin().disable()
